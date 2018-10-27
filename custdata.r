ba89d6a6a7244b57a402dcbe1975507a45bd3117
Dhiraj <dhiraj.jamia@gmail.com>
2018-10-27 23:18:42 +0530

    Update custdata.r

--- custdata.r
+++ custdata.r
@@ -1,7 +1,15 @@
 install.packages("dplyr")
 install.packages("tidyr")
 install.packages("ggplot2") 
-libarary('dplyr')
-libarary('tidyr')
-libarary('ggplot2')
+library('dplyr')
+library('tidyr')
+library('ggplot2')
 data_set<-read.table(fill.choose(),header=T,sep='\t')
+View(data_set)
+summary(data_set)
+# Clean all missing raws in the data set
+data_set_new<-data_set %>% drop_na()
+View(data_set_new)
+summary(data_set_new)
+
+
