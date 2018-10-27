c72735619047d78b1cbf69cf5e0a14065477a629
Dhiraj <dhiraj.jamia@gmail.com>
2018-10-27 23:04:36 +0530

    Create custdata.r

---
+++ custdata.r
@@ -0,0 +1,7 @@
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("ggplot2") 
+libarary('dplyr')
+libarary('tidyr')
+libarary('ggplot2')
+data_set<-read.table(fill.choose(),header=T,sep='\t')
