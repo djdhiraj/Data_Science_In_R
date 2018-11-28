4b350fc199c1e719ee26fca3d26b36eaccba322d
Dhiraj <dhiraj.jamia@gmail.com>
2018-11-28 11:21:17 +0530

    Update Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -20,3 +20,40 @@ ROCRpred<-prediction(fitted_result,test$vs)
 ROCRPref<-performance(ROCRpred,measure = "tpr",x.measure = "fpr")
 plot(ROCRPref)
 plot(ROCRPref,colorize="TRUE")
+######################################################################
+######################################################################
+Model_2
+######################################################################
+######################################################################
+# Data set Telco-Customer-Churn#######################################
+View(data_set)
+data_set$Churn<-as.factor(data_set$Churn)
+str(data_set)
+data_set_preprocess<-ifels(data_set$Churn=="Yes",1,0)
+data_set_preprocess<-ifelse(data_set$Churn=="Yes",1,0)
+data_set_preprocess
+View(data_set_preprocess)
+data_set_preprocess<-data_set
+data_set_preprocess$Churn<-ifelse(data_set$Churn=="Yes",1,0)
+data_set_preprocess<-subset(data_set_preprocess,select=-customerID)
+split<-sample.split(data_set_preprocess,SplitRatio = 0.75)
+View(train)
+View(train)
+train<-subset(data_set_preprocess,split=="TRUE")
+test<-subset(data_set_preprocess,split=="FALSE")
+View(train)
+model<-glm(Churn~.,data=train)
+Final_model<-stepAIC(model,direction = "both")
+summary(First_model)
+test$Churn_predict<-predict(First_model,test,type="response")
+View(test)
+test$Churn_predict<-ifelse(test$Churn_predict>0.5,1,0)
+View(test)
+table<-table(test$Churn,test$Churn_predict)
+table
+install.packages("caret")
+library(ceret)
+library(caret)
+Confusion_Matrix<-confusionMatrix(table)
+Confusion_Matrix
+
