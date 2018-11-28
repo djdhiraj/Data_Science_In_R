2883c55cd4506fb587a46e8129bf79ed8b46b5db
Dhiraj <dhiraj.jamia@gmail.com>
2018-11-28 12:10:00 +0530

    Update Support_Vector_Machine.R

--- Support_Vector_Machine.R
+++ Support_Vector_Machine.R
@@ -1,3 +1,13 @@
+View(data_set)
+data_set_preprocess<-data_set
+data_set_preprocess<-subset(data_set_preprocess,select=-customerID)
+data_set_preprocess$Churn<-ifelse(data_set$Churn=="Yes",1,0)
+#train<-subset(data_set_preprocess,split=="Yes")
+test<-subset(data_set_preprocess,slit="FALSE")
+train<-subset(data_set_preprocess,slit="TRUE")
+View(train)
+View(test)
+#train_scale<-scale(train[,1,15])
 library(e1071)
 model_svm<-svm(Churn~.,data=train,method="class")
 summary(model_svm)
