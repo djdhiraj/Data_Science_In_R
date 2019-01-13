15454e09c4270295a95d254700f583d3b6315edf
Dhiraj <dhiraj.jamia@gmail.com>
2019-01-13 22:53:37 +0530

    Update and rename Chapter_17_Text_Mining.R to Chapter_18_Text_Mining.R

--- R_Files/Chapter_18_Text_Mining.R
+++ R_Files/Chapter_18_Text_Mining.R
@@ -32,7 +32,7 @@ train.tokens<-tokens_select(train.tokens,stopwords(),selection = "remove")
 train.tokens[[20]]
 train.tokens<-tokens_wordstem(train.tokens,language = "english")
 train.tokens[[20]]
-train.tokens.dfm<-dfm(train.tokens,tolower = FALSE,remove=stopwords())
+train.tokens.dfm<-dfm(train.tokens,tolower = FALSE)
 train.tokens.matrix<-as.matrix(train.tokens.dfm)
 #View(train.tokens.dfm)[1:10,1:10]
 dim(train.tokens.matrix)
@@ -43,8 +43,55 @@ names(train.tokens.df)<-make.names(names(train.tokens.df))
 set.seed(46532)
 cv.folds<-createMultiFolds(train$label,k=10,times=3)
 cv.cntrl<-trainControl(method="repeatedcv",number=10,repeats = 3,index=cv.folds)
-install.packages("doSNOW")
+#install.packages("doSNOW")
 library(doSNOW)
 start.time<-Sys.time()
+cl<-makeCluster(2,type="SOCK")
+registerDoSNOW(cl)
+
+total.time<-Sys.time()-start.time
+total.time
 rpart.cv.1<-train(lable~.,data=train.tokens.df,method="rpart",
                   trControl=cv.cntrl,tuneLength=7)
+
+
+stopCluster(cl)
+total.time<-Sys.time()-start.time
+total.time
+term.frequency<-function(row){
+  row/sum(row)
+}
+inverse.doc.freq<-function(col){
+  corpus.size<-length(col)
+  doc.count<-length(which(col>0))
+  log10(corpus.size/doc.count)
+}
+tf.ide<-function(x,ide){
+  x*idf
+}
+train.tokens.df<-apply(train.token.matrix,1,term.frequency)
+dim<-(train.tokens.df)
+View(train.tokens.df[1:10,1:10])
+train.tokens.idf<-apply(train.tokens.matrix,2,inverse.doc.freq)
+str(train.tokens.idf)
+train_tokens.tfidf<-apply(train.tokens.df,2,tf.ide,idf=train.tokens.idf)
+dim(train_tokens.tfidf)
+View(train.tokens.tfidf[1:25,1:25])
+incomplete_cases<-which(!complete.cases(train_tokens.tfidf))
+train$text[incomplete_cases]
+train.tokens.tfidf[incomplete_cases,]<-rep(0,0,ncol(train_tokens.tfidf))
+dim(train_tokens.tfidf)
+sum(which(!complete.cases(train_tokens.tfidf)))
+train_tokens.tfidf.df<-cbind(Lable=train$Lable,data.frame(train_tokens.tfidf))
+names(train_tokens.tfidf.df)<-make.names(names(train_tokens.tfidf.df))
+start.time<-Sys.time()
+cl<-makeCluster(3,type="SOCK")
+registerDoSNOW(cl)
+rpart.cv.2<-train(Lable~.,data=train.tokens.tfidf.df,method="rpart",
+                  trControl=cv.cntrl,turnLength=6)
+stopCluster(cl)
+total.time<-Sys.time()-start.time
+total.time
+rpart.cv.2
+train.tokens<-tokens_ngrams(train.tokens,n=1:2)
+
