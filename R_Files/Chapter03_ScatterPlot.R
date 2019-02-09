4bf78a8542689c87f14072885a292e4762f390ac
Dhiraj <dhiraj.jamia@gmail.com>
2019-02-09 18:15:25 +0530

    Update Chapter03_ScatterPlot.R

--- R_Files/Chapter03_ScatterPlot.R
+++ R_Files/Chapter03_ScatterPlot.R
@@ -33,3 +33,30 @@ lines(t, type = "o", col = "blue")
 
 # Save the file.
 dev.off()
+
+
+data<-read.csv(file.choose())
+tsData <- EuStockMarkets[, 1] # ts data
+decomposedRes <- decompose(tsData1) # use type = "additive" for additive components
+plot (tsData1)
+plot(tsData2)
+View(tsData)
+tsData1<-data[,1:2]
+tsData2<-data[,1:3]
+View(tsData1)
+attach(mtcars)
+par(mfrow=c(2,2))
+plot(wt,mpg, main="Scatterplot of wt vs. mpg")
+plot(wt,disp, main="Scatterplot of wt vs disp")
+hist(wt, main="Histogram of wt")
+boxplot(wt, main="Boxplot of wt")
+plot(Class.1,type="o",main="Class 1")
+plot(Class.2,typ="o",main="Class 2")
+plot(Class.3,type="o",main="Class 3")
+plot(Class.4,type="o",main="class 4")
+plot(Class.5,type="o",main="class 5")
+plot(Class.6,type="o",main="class 6",col="Green")
+attach(data)
+lines(Class.1,col="Blue")
+lines(Class.2,col="red")
+
