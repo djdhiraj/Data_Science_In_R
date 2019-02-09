9de8ca0ae85a3c186d827469d59f03907b6dc0e1
Dhiraj <dhiraj.jamia@gmail.com>
2019-02-09 18:08:28 +0530

    Update Chapter03_ScatterPlot.R

--- R_Files/Chapter03_ScatterPlot.R
+++ R_Files/Chapter03_ScatterPlot.R
@@ -16,3 +16,20 @@ gg <- ggplot(midwest, aes(x=area, y=poptotal)) +
        caption = "Source: midwest")
 
 plot(gg)
+
+#Multiple Lines in a Line Chart
+# Create the data for the chart.
+v <- c(7,12,28,3,41)
+t <- c(14,7,6,19,3)
+
+# Give the chart file a name.
+png(file = "line_chart_2_lines.jpg")
+
+# Plot the bar chart.
+plot(v,type = "o",col = "red", xlab = "Month", ylab = "Rain fall", 
+   main = "Rain fall chart")
+
+lines(t, type = "o", col = "blue")
+
+# Save the file.
+dev.off()
